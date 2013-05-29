97e3210056288ddf8ea72160f30dc844d373287b
carlosTJunior <carlosjunior117@hotmail.com>
2013-05-29 20:33:19 -0300

    classe MonitorTeste

---
+++ testes/sistemaacademico/entidades/MonitorTeste.java
@@ -0,0 +1,59 @@
+package sistemaacademico.entidades;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MonitorTeste {
+
+	@Test
+	public void testMonitor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMonitorStringStringStringStringStringIntStringStringStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMencao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMencao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDisciplinaMonitoria() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDisciplinaMonitoria() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHorario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHorario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProfessor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProfessor() {
+		fail("Not yet implemented");
+	}
+
+}
